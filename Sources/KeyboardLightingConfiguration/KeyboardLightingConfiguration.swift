5980896c35578d5599ce34e0ea6c04f0552b490a
Maxime CHAPELET <maxime.chapelet@nahimic.com>
2020-01-16 22:02:13 +0100

    Define KeyboardConfiguration protocol and KeyConfiguration structure

--- Sources/KeyboardLightingConfiguration/KeyboardLightingConfiguration.swift
+++ Sources/KeyboardLightingConfiguration/KeyboardLightingConfiguration.swift
@@ -1,3 +1,22 @@
-struct KeyboardLightingConfiguration {
-    var text = "Hello, World!"
+public struct KeyConfiguration {
+    enum Effect {
+        case Static
+        case Disco
+        case Wave
+    }
+    enum Color {
+        case Red
+        case Green
+        case Blue
+        case Orange
+        case Yellow
+    }
+    let key: String
+    let effect: Effect
+    let color: Color
+}
+
+public protocol KeyboardConfiguration {
+    var keyConfigurations: [String: KeyConfiguration] {get}
+    func from(inputParsingResult:InputParsingResult)
 }
