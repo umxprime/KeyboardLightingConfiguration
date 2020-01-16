5ad5b15d75fbfbe6f9602f3dd38023a5f971bb41
Maxime CHAPELET <maxime.chapelet@nahimic.com>
2020-01-16 22:07:57 +0100

    Define OutputEncoder protocol

---
+++ Sources/KeyboardLightingConfiguration/OutputEncoder.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Maxime CHAPELET on 16/01/2020.
+//
+
+import Foundation
+
+
+public typealias OutputEncodingResult = String
+
+public protocol OutputEncoder {
+    func encode(configuration: KeyboardConfiguration) throws -> OutputEncodingResult
+}
