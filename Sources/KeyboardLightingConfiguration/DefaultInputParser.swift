feab34817758683fa87c67d2428742d6a4bec20b
Maxime CHAPELET <maxime.chapelet@nahimic.com>
2020-01-16 12:10:32 +0100

    Implement a simple input parsing to pass simple valid entry found test

--- Sources/KeyboardLightingConfiguration/DefaultInputParser.swift
+++ Sources/KeyboardLightingConfiguration/DefaultInputParser.swift
@@ -13,9 +13,23 @@ public class DefaultInputParser {
 
 extension DefaultInputParser: InputParser {
     public func parse(input: String) throws {
-        if (input.isEmpty) {
+        if input.isEmpty {
             throw InputParsingError(kind: .EmptyInput)
         }
-        throw InputParsingError(kind: .NoValidEntryFound)
+        let pattern = #"""
+        (?xm)
+        ^(?<key>
+            a)\n
+        (?<type>
+            static)\n
+        (?<color>
+            red)
+        """#
+        let regex = try NSRegularExpression(pattern: pattern, options: [])
+        let range = NSRange(input.startIndex..<input.endIndex, in: input)
+        let matches = regex.matches(in: input, options: [], range: range)
+        if matches.count == 0 {
+            throw InputParsingError(kind: .NoValidEntryFound)
+        }
     }
 }
