15bccbd917654c1cd804fb9554729dbc400dad78
Maxime CHAPELET <maxime.chapelet@nahimic.com>
2020-01-16 12:29:18 +0100

    Change pattern to make all tests pass

--- Sources/KeyboardLightingConfiguration/DefaultInputParser.swift
+++ Sources/KeyboardLightingConfiguration/DefaultInputParser.swift
@@ -19,7 +19,7 @@ extension DefaultInputParser: InputParser {
         let pattern = #"""
         (?xm)
         ^(?<key>
-            a)\n
+            (?:(?:a|b)(?-x:$|, ))+)\n
         (?<type>
             static)\n
         (?<color>
